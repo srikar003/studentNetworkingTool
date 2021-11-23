7c7c2c408f266d9884c7ae6499d99d87d59e0ccb
srikar003 <srikarvuppala003@gmail.com>
2021-11-23 04:09:36 -0600

    updated db names and few query changes

--- createCommands.sql
+++ createCommands.sql
@@ -15,7 +15,7 @@ create table IF NOT EXISTS instructorRegistration(
 );
 
 create table IF NOT EXISTS students(
-    studentId varchar(16),
+    studentId varchar(36),
     firstName varchar(20),
     lastName varchar(20),
     email varchar(30),
@@ -26,27 +26,27 @@ create table IF NOT EXISTS students(
 );
 
 create table IF NOT EXISTS courses(
-    courseId varchar(16),
-    courseName varchar(40),
+    courseId varchar(36),
+    courseName varchar(36),
     isPaidResource boolean,
     primary key(courseId)
 );
 
 create table IF NOT EXISTS studentContact(
     contactNo numeric(10),
-    studentId varchar(16),
+    studentId varchar(36),
     primary key(contactNo),
     foreign key(studentId) REFERENCES students(studentId)
 );
 
 create table IF NOT EXISTS events(
-    eventId varchar(16),
+    eventId varchar(36),
     eventName varchar(20),
     primary key(eventId)
 );
 
 create table IF NOT EXISTS instructors(
-    instructorId varchar(16),
+    instructorId varchar(36),
     teachingExperience numeric(2),
     firstName varchar(20),
     lastName varchar(20),
@@ -59,76 +59,76 @@ create table IF NOT EXISTS instructors(
 
 create table IF NOT EXISTS instructorContact(
     contactNo numeric(10),
-    instructorId varchar(16),
+    instructorId varchar(36),
     primary key(contactNo),
     foreign key(instructorId) REFERENCES instructors(instructorId)
 );
 
 create table IF NOT EXISTS enrollment(
-    studentId varchar(16),
-    courseId varchar(16),
+    studentId varchar(36),
+    courseId varchar(36),
     foreign key(studentId) REFERENCES students(studentId),
     foreign key(courseId) REFERENCES courses(courseId)
 );
 
 create table IF NOT EXISTS eventParticipation(
-    studentId varchar(16),
-    eventId varchar(16),
+    studentId varchar(36),
+    eventId varchar(36),
     foreign key(studentId) REFERENCES students(studentId),
     foreign key(eventId) REFERENCES events(eventId)
 );
 
 create table IF NOT EXISTS teaches(
-    instructorId varchar(16),
-    courseId varchar(16),
+    instructorId varchar(36),
+    courseId varchar(36),
     foreign key(instructorId) REFERENCES instructors(instructorId),
     foreign key(courseId) REFERENCES courses(courseId)
 );
 
 create table IF NOT EXISTS corporateProfessionals(
-    instructorId varchar(16),
+    instructorId varchar(36),
     companyName varchar(20),
     foreign key(instructorId) REFERENCES instructors(instructorId)
 );
 
 create table IF NOT EXISTS professors(
-    instructorId varchar(16),    
+    instructorId varchar(36),    
     universityName varchar(50),
     foreign key(instructorId) REFERENCES instructors(instructorId)
 );
 
 create table IF NOT EXISTS queries(
-    queryId varchar(16),
+    queryId varchar(36),
     question varchar(100),
-    studentId varchar(16),
-    courseId varchar(16),
+    studentId varchar(36),
+    courseId varchar(36),
     primary key(queryId),
     foreign key(studentId) REFERENCES students(studentId),
     foreign key(courseId) REFERENCES courses(courseId)
 );
 
 create table IF NOT EXISTS queryResponses(
-    instructorId varchar(16),
+    instructorId varchar(36),
     response varchar(100),    
-    queryId varchar(16),
+    queryId varchar(36),
     foreign key(queryId) REFERENCES queries(queryId),
     foreign key(instructorId) REFERENCES instructors(instructorId)
 );
 
 create table IF NOT EXISTS queryTags(
     hashtags varchar(10),
-    queryId varchar(16),
+    queryId varchar(36),
     primary key(hashtags),
     foreign key(queryId) REFERENCES queries(queryId)
 );
 
 create table IF NOT EXISTS payment(
-    transactionId varchar(16),
+    transactionId varchar(36),
     paymentType varchar(20),
     amount numeric(6),
     isTransactionSuccessful boolean,
-    studentId varchar(16),
-    courseId varchar(16),
+    studentId varchar(36),
+    courseId varchar(36),
     primary key(transactionId),
     foreign key(studentId) REFERENCES students(studentId),
     foreign key(courseId) REFERENCES courses(courseId)
@@ -136,19 +136,19 @@ create table IF NOT EXISTS payment(
 
 create table IF NOT EXISTS cardPayment(
     cardHolder varchar(20),
-    cardNo numeric(16),
+    cardNo numeric(36),
     expiryMonth numeric(2),
     expiryYear numeric(2),
     cvv numeric(4),
-    transactionId varchar(16),
+    transactionId varchar(36),
     foreign key(transactionId) REFERENCES payment(transactionId)
 );
 
 create table IF NOT EXISTS electronicCheck(
     accountHolder varchar(20),
-    routingNo numeric(16),
+    routingNo numeric(36),
     bankName varchar(20),
     branch varchar(20),
-    transactionId varchar(16),
+    transactionId varchar(36),
     foreign key(transactionId) REFERENCES payment(transactionId)
 );
--- src/main/java/com/example/StudentNetworkingTool/StudentNetworkingToolApplication.java
+++ src/main/java/com/example/StudentNetworkingTool/StudentNetworkingToolApplication.java
@@ -1,7 +1,7 @@
 package com.example.StudentNetworkingTool;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
-public class StudentNetworkingToolApplication  implements CommandLineRunner{
+public class StudentNetworkingToolApplication{
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -18,20 +18,18 @@ public class StudentNetworkingToolApplication  implements CommandLineRunner{
         SpringApplication.run(StudentNetworkingToolApplication.class, args);
     }
  
-    @Override
-    public void run(String... args) throws Exception {
-        String sql = "INSERT INTO actor (actor_id, first_name, last_name) VALUES (?, ?, ?)";
-         
-        int result = jdbcTemplate.update(sql, 201, "ravi", "kumar");
+    @GetMapping("/run")
+    public void run(String... args){
+        String sql = "INSERT INTO courses (courseId, courseName, isPaidResource) VALUES (?, ?, ?)";
+        
+        UUID corrId = UUID.randomUUID();
+        String corrIdAsString = corrId.toString();
+        System.out.println(corrIdAsString.length());
+        int result = jdbcTemplate.update(sql,corrIdAsString , "Java", false);
          
         if (result > 0) {
             System.out.println("A new row has been inserted.");
         }
          
     }
-
-	@GetMapping("/hello")
-		public String hello() {
-		return "Hello Worlds";
-	}
 }
