f209732590db978ef82a9a7782c88f43115de189
likith2897 <94898289+likith2897@users.noreply.github.com>
2021-11-23 04:00:50 -0600

    Add files via upload

---
+++ insert_cmnds.sql
@@ -0,0 +1,47 @@
+insert into studentRegistration values ('pearson','clear lake'),('mcgraw','katy'),('dolphin','houston');
+
+
+insert into instructorRegistration values ('tom','boston'),('lulu','denver'),('abcd','miami');
+
+insert into students values ('111','joseph','boston','jb@gmail.com','20-10-1995'),('222','john','austin','ja@gmail.com','20-10-1995'),('333','mike','dallas','md@gmail.com','20-10-1995');
+
+insert into courses values ('10','dbms',0),('11','cpsm',1),('12','rm',0);
+
+insert into studentContact values (1234567891,111),(985647389,222),(362857395890,333);
+
+insert into events values ('one','code'),('two','test'),('three','develop');
+
+insert into instructors values ('101',5,'kh','ha','kh@gmail.com','20-10-1995','tom'),('102',3,'hi','md','himd@gmail.com','20-11-1995','lulu'),('103',2,'te','fi','tefi@gmail.com','10-10-1998','abcd');
+
+insert into instructorContact values (78392837468,101),(183947374859,102),(94827364987,103);
+
+insert into enrollment values ('10','111'),('11','222'),('12','333');
+
+insert into eventParticipation values ('10','one'),('11','two'),('12','three');
+
+insert into corporateProfessionals values ('101','cg'),('102','ag'),('103','og');
+
+insert into professors values ('101','uhcl'),('102','uhcl'),('103','uhcl');
+
+insert into queries values ('55','abc','111','10'),('66','def','222','11'),('77','ghi','333','12');
+
+insert into queryResponses values ('101','hhh','55'),('102','ccc','66'),('103','iii','77');
+
+insert into queryTags values ('python','55'),('cad','66'),('dbms','77');
+
+insert into payment values (11111,'card',100,0,'111','10'),(22222,'card',200,0,'222','11'),(33333,'card',300,0,'333','12');
+
+insert into cardPayment values ('xxxx',12345,11,2025,532,11111),('aaaa',8962849,08,2029,410,22222),('bhiaes',7284722,03,2033,253,33333);
+
+insert into electronicCheck values ('bhui',12345,'chase','cl',11111),('wscd',8962849,'boa','ho',22222),('lkhn',7284722,'pnc','kt',33333);
+
+
+
+
+
+
+
+
+
+
+
