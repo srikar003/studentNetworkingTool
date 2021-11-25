b1695aa9195b8aa05351871cc41277435161953c
srikar003 <srikarvuppala003@gmail.com>
2021-11-25 13:45:10 -0600

    updated create commands

--- createCommands.sql
+++ createCommands.sql
@@ -1,21 +1,21 @@
-create database studentNetworkingTool;
+create database IF NOT EXISTS studentNetworkingTool;
 
 use studentNetworkingTool;
 
-create table studentRegistration(
+create table IF NOT EXISTS studentRegistration(
     userName varchar(10) not null,
     password varchar(20) not null,
     primary key(userName)
 );
 
-create table instructorRegistration(
+create table IF NOT EXISTS instructorRegistration(
     userName varchar(10) not null,
     password varchar(20) not null,
     primary key(userName)
 );
 
-create table students(
-    studentId varchar(16) not null,
+create table IF NOT EXISTS students(
+    studentId varchar(36) not null,
     firstName varchar(20) not null,
     lastName varchar(20),
     email varchar(30) not null,
@@ -24,31 +24,32 @@ create table students(
     department varchar(15) not null,
     userName varchar(10) not null,
     primary key(studentId),
-    foreign key(userName) REFERENCES studentRegistration(userName) on delete cascade on update cascade
+    foreign key(userName) REFERENCES studentRegistration(userName)
 );
 
-create table courses(
-    courseId varchar(16) not null,
+create table IF NOT EXISTS courses(
+    courseId varchar(36) not null,
     courseName varchar(40) not null,
-    isPaidResource boolean not null,
+    isPaidCourse boolean not null,
+    price numeric(5),
     primary key(courseId)
 );
 
-create table studentContact(
+create table IF NOT EXISTS studentContact(
     contactNo numeric(10),
-    studentId varchar(16),
+    studentId varchar(36),
     primary key(contactNo),
-    foreign key(studentId) REFERENCES students(studentId) on delete cascade on update cascade
+    foreign key(studentId) REFERENCES students(studentId)
 );
 
-create table events(
-    eventId varchar(16),
+create table IF NOT EXISTS events(
+    eventId varchar(36),
     eventName varchar(20),
     primary key(eventId)
 );
 
-create table instructors(
-    instructorId varchar(16) not null,
+create table IF NOT EXISTS instructors(
+    instructorId varchar(36) not null,
     teachingExperience numeric(2),
     firstName varchar(20) not null,
     lastName varchar(20),
@@ -56,101 +57,101 @@ create table instructors(
     dateOfBirth date,
     userName varchar(10) not null,
     primary key(instructorId),
-    foreign key(userName) REFERENCES instructorRegistration(userName) on delete cascade on update cascade
+    foreign key(userName) REFERENCES instructorRegistration(userName)
 );
 
-create table instructorContact(
+create table IF NOT EXISTS instructorContact(
     contactNo numeric(10),
-    instructorId varchar(16),
+    instructorId varchar(36),
     primary key(contactNo),
-    foreign key(instructorId) REFERENCES instructors(instructorId) on delete cascade on update cascade
+    foreign key(instructorId) REFERENCES instructors(instructorId)
 );
 
-create table enrollment(
-    studentId varchar(16),
-    courseId varchar(16),
-    foreign key(studentId) REFERENCES students(studentId) on delete cascade on update cascade,
-    foreign key(courseId) REFERENCES courses(courseId) on delete cascade on update cascade
+create table IF NOT EXISTS enrollment(
+    studentId varchar(36),
+    courseId varchar(36),
+    foreign key(studentId) REFERENCES students(studentId),
+    foreign key(courseId) REFERENCES courses(courseId)
 );
 
-create table eventParticipation(
-    studentId varchar(16),
-    eventId varchar(16),
-    foreign key(studentId) REFERENCES students(studentId) on delete cascade on update cascade,
-    foreign key(eventId) REFERENCES events(eventId) on delete cascade on update cascade
+create table IF NOT EXISTS eventParticipation(
+    studentId varchar(36),
+    eventId varchar(36),
+    foreign key(studentId) REFERENCES students(studentId),
+    foreign key(eventId) REFERENCES events(eventId)
 );
 
-create table teaches(
-    instructorId varchar(16) not null,
-    courseId varchar(16) not null,
-    foreign key(instructorId) REFERENCES instructors(instructorId) on delete cascade on update cascade,
-    foreign key(courseId) REFERENCES courses(courseId) on delete cascade on update cascade
+create table IF NOT EXISTS teaches(
+    instructorId varchar(36) not null,
+    courseId varchar(36) not null,
+    foreign key(instructorId) REFERENCES instructors(instructorId),
+    foreign key(courseId) REFERENCES courses(courseId)
 );
 
-create table corporateProfessionals(
-    instructorId varchar(16) not null,
+create table IF NOT EXISTS corporateProfessionals(
+    instructorId varchar(36) not null,
     companyName varchar(20) not null,
-    foreign key(instructorId) REFERENCES instructors(instructorId) on delete cascade on update cascade
+    foreign key(instructorId) REFERENCES instructors(instructorId)
 );
 
-create table professors(
-    instructorId varchar(16) not null,
+create table IF NOT EXISTS professors(
+    instructorId varchar(36) not null,
     universityName varchar(50) not null,
-    foreign key(instructorId) REFERENCES instructors(instructorId) on delete cascade on update cascade
+    foreign key(instructorId) REFERENCES instructors(instructorId)
 );
 
-create table queries(
-    queryId varchar(16),
-    question varchar(100),
-    studentId varchar(16),
-    courseId varchar(16),
+create table IF NOT EXISTS queries(
+    queryId varchar(36),
+    question varchar(100) not null,
+    studentId varchar(36),
+    courseId varchar(36),
     primary key(queryId),
-    foreign key(studentId) REFERENCES students(studentId) on delete cascade on update cascade,
-    foreign key(courseId) REFERENCES courses(courseId) on delete cascade on update cascade
+    foreign key(studentId) REFERENCES students(studentId),
+    foreign key(courseId) REFERENCES courses(courseId)
 );
 
-create table queryResponses(
-    instructorId varchar(16),
-    response varchar(100),
-    queryId varchar(16),
-    foreign key(queryId) REFERENCES queries(queryId) on delete cascade on update cascade,
-    foreign key(instructorId) REFERENCES instructors(instructorId) on delete cascade on update cascade
+create table IF NOT EXISTS queryResponses(
+    instructorId varchar(36),
+    response varchar(100) not null,
+    queryId varchar(36),
+    foreign key(queryId) REFERENCES queries(queryId),
+    foreign key(instructorId) REFERENCES instructors(instructorId)
 );
 
-create table queryTags(
+create table IF NOT EXISTS queryTags(
     hashtags varchar(10),
-    queryId varchar(16),
+    queryId varchar(36),
     primary key(hashtags),
-    foreign key(queryId) REFERENCES queries(queryId) on delete cascade on update cascade
+    foreign key(queryId) REFERENCES queries(queryId)
 );
 
-create table payment(
+create table IF NOT EXISTS payment(
     transactionId numeric(30) not null,
     paymentType varchar(20) not null,
     amount numeric(6),
     isTransactionSuccessful boolean not null,
-    studentId varchar(16) not null,
-    courseId varchar(16),
+    studentId varchar(36) not null,
+    courseId varchar(36),
     primary key(transactionId),
-    foreign key(studentId) REFERENCES students(studentId) on delete cascade on update cascade,
-    foreign key(courseId) REFERENCES courses(courseId) on delete cascade on update cascade
+    foreign key(studentId) REFERENCES students(studentId),
+    foreign key(courseId) REFERENCES courses(courseId)
 );
 
-create table cardPayment(
+create table IF NOT EXISTS cardPayment(
     cardHolder varchar(20),
-    cardNo numeric(16),
+    cardNo numeric(36),
     expiryMonth numeric(2),
     expiryYear numeric(2),
     cvv numeric(4),
-    transactionId numeric(30),
-    foreign key(transactionId) REFERENCES payment(transactionId) on delete cascade on update cascade
+    transactionId varchar(36),
+    foreign key(transactionId) REFERENCES payment(transactionId)
 );
 
-create table electronicCheck(
+create table IF NOT EXISTS electronicCheck(
     accountHolder varchar(20),
-    routingNo numeric(16),
+    routingNo numeric(36),
     bankName varchar(20),
     branch varchar(20),
-    transactionId numeric(30),
-    foreign key(transactionId) REFERENCES payment(transactionId) on delete cascade on update cascade
+    transactionId varchar(36),
+    foreign key(transactionId) REFERENCES payment(transactionId)
 );
